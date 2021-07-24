265c739ba82950308d9fca3cf6541e157b9083e1
max0l0gy <maxmorev@gmail.com>
2021-07-24 13:10:00 +0300

    Added reactive feign options

--- src/main/kotlin/ru/max0l0gy/customer/report/collector/CustomerReportCollectorApplication.kt
+++ src/main/kotlin/ru/max0l0gy/customer/report/collector/CustomerReportCollectorApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import reactivefeign.spring.config.EnableReactiveFeignClients
 
-@EnableReactiveFeignClients
 @SpringBootApplication
+@EnableReactiveFeignClients
 class CustomerReportCollectorApplication
 
 fun main(args: Array<String>) {
