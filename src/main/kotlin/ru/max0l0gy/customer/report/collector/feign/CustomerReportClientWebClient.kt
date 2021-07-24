4b3623d7bf90aea2b8ba4cfaded042273fb02a2f
max0l0gy <maxmorev@gmail.com>
2021-07-24 16:18:46 +0300

    Added reactive web client

---
+++ src/main/kotlin/ru/max0l0gy/customer/report/collector/feign/CustomerReportClientWebClient.kt
@@ -0,0 +1,35 @@
+package ru.max0l0gy.customer.report.collector.feign
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+import ru.max0l0gy.customer.report.collector.domain.Report
+import ru.max0l0gy.customer.report.collector.domain.RestResponse
+
+
+@Component
+class CustomerReportClientWebClient(
+    val webClient: WebClient,
+    @Value("\${external.customer-report-calculation.reports}")
+    private var url: String
+) : CustomerReportClient {
+
+    override fun reportBy(customerId: Long): Mono<RestResponse<Report>> {
+        val typeRef: ParameterizedTypeReference<RestResponse<Report>> = object : ParameterizedTypeReference<RestResponse<Report>>() {}
+        val urlWithParams : String = url.replace("{customerId}", customerId.toString())
+        
+        return webClient
+            .get()
+            .uri(urlWithParams)
+            .attribute("customerId", customerId)
+            .accept(APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono(typeRef)
+    }
+
+
+}
+
